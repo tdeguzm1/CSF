05d2432822c33c4b489db1fbe0ae509312eb1196
tdeguzm1 <tdeguzm1@jh.edu>
2022-02-18 15:30:09 -0500

    working 27/30

--- hw2/c_hexfuncs.c
+++ hw2/c_hexfuncs.c
@@ -63,7 +63,7 @@ void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]){
 // unmodified.  If byteval is not a printable character, then the
 // ASCII code for '.' should be returned.
 char hex_to_printable(unsigned char byteval){
-   if (byteval < 32 || byteval == 127 || byteval > 254){
+   if (byteval < 32 || byteval >= 127){
        return '.';
    }
    return byteval;
--- hw2/hextests.c
+++ hw2/hextests.c
@@ -55,12 +55,56 @@ void testFormatOffset(TestObjs *objs) {
 
 void testFormatByteAsHex(TestObjs *objs) {
   char buf[16];
+  
+  
+  //Hello,
   hex_format_byte_as_hex(objs->test_data_1[0], buf);
-  //printf("%s\n", buf);
   ASSERT(0 == strcmp(buf, "48"));
+  hex_format_byte_as_hex(objs->test_data_1[1], buf);
+  ASSERT(0 == strcmp(buf, "65"));
+  hex_format_byte_as_hex(objs->test_data_1[2], buf);
+  ASSERT(0 == strcmp(buf, "6c"));
+  hex_format_byte_as_hex(objs->test_data_1[3], buf);
+  ASSERT(0 == strcmp(buf, "6c"));
+  hex_format_byte_as_hex(objs->test_data_1[4], buf);
+  ASSERT(0 == strcmp(buf, "6f"));
+  hex_format_byte_as_hex(objs->test_data_1[5], buf);
+  ASSERT(0 == strcmp(buf, "2c"));
+
+  // world!
+  hex_format_byte_as_hex(objs->test_data_1[6], buf);
+  //printf("Start value: \"%s\"\n", buf);
+  ASSERT(0 == strcmp(buf, "20"));
+  
+  hex_format_byte_as_hex(objs->test_data_1[7], buf);
+  ASSERT(0 == strcmp(buf, "77")); // w
+  hex_format_byte_as_hex(objs->test_data_1[8], buf);
+  ASSERT(0 == strcmp(buf, "6f")); // o
+  hex_format_byte_as_hex(objs->test_data_1[9], buf);
+  ASSERT(0 == strcmp(buf, "72")); // r
+  hex_format_byte_as_hex(objs->test_data_1[10], buf);
+  ASSERT(0 == strcmp(buf, "6c")); // l
+  hex_format_byte_as_hex(objs->test_data_1[11], buf);
+  ASSERT(0 == strcmp(buf, "64")); // d
+  hex_format_byte_as_hex(objs->test_data_1[12], buf);
+  ASSERT(0 == strcmp(buf, "21")); // !
+  
+  hex_format_byte_as_hex(objs->test_data_1[13], buf);
+  //printf("Start value: \"%s\"\n", buf);
+  ASSERT(0 == strcmp(buf, "0a")); // \n
+  
+  hex_format_byte_as_hex(objs->test_data_1[14], buf);
+  //printf("Start value: \"%s\"\n", buf);
+  ASSERT(0 == strcmp(buf, "00")); // \0
+  
+
+
+
 }
 
 void testHexToPrintable(TestObjs *objs) {
+  //printf("%d\n", hex_to_printable(objs->test_data_1[0]));
+  //printf("%d\n", hex_to_printable(objs->test_data_1[13]));
   ASSERT('H' == hex_to_printable(objs->test_data_1[0]));
   ASSERT('.' == hex_to_printable(objs->test_data_1[13]));
 }
